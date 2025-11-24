c401fc95d7d9cefc239720deee2b32ba3ce90987
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2025-11-24 23:58:32 +0530

    Implement removeNthFromEnd function in Solution.cpp

--- LeetCode/Remove-Nth-Node-From-End-of-List/Solution.cpp
+++ LeetCode/Remove-Nth-Node-From-End-of-List/Solution.cpp
@@ -1,2 +1,25 @@
-head = [1,2]
-n = 2
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* temp = head;
+        int len = 0;
+        while(temp!=nullptr){
+            len++;
+            temp = temp->next;
+        }
+        len = len-n;
+        if(len == 0){
+            ListNode* newHead = head->next;
+            delete head;
+            return newHead;
+        }
+        temp = head;
+        for(int i=1; i<len; i++){
+            temp = temp->next;
+        }
+        ListNode* delnode = temp->next;
+        temp->next = temp->next->next;
+        delete delnode;
+        return head;
+    }
+};
