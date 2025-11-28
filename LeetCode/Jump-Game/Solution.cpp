128488b50ba2a7c777c04ddde135f5d362a95646
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2025-11-28 23:16:33 +0530

    Fix formatting and ensure newline at end of file

--- LeetCode/Jump-Game/Solution.cpp
+++ LeetCode/Jump-Game/Solution.cpp
@@ -1,12 +1,12 @@
-1class Solution {
-2public:
-3    bool canJump(vector<int>& nums) {
-4        int farthest = 0;
-5        for(int i=0; i<nums.size(); i++){
-6            if(i>farthest) return false;
-7            farthest = max(farthest , i + nums[i]);
-8            if(farthest >= nums.size()-1)return true;
-9        }
-10        return true;
-11    }
-12};
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int farthest = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(i>farthest) return false;
+            farthest = max(farthest , i + nums[i]);
+            if(farthest >= nums.size()-1)return true;
+        }
+        return true;
+    }
+};
