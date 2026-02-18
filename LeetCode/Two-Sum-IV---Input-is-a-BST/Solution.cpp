63281f631e7e81c538fe06e68aabb76c8a83219d
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2026-02-18 13:40:16 +0530

    Add solution for Two Sum IV - Input is a BST

--- LeetCode/Two-Sum-IV---Input-is-a-BST/Solution.cpp
+++ LeetCode/Two-Sum-IV---Input-is-a-BST/Solution.cpp
@@ -1,17 +1,24 @@
-[2,1,3]
-1
-
-[2,1,3]
-3
-
-[2,1,3]
-4
-
-[2,0,3,-4,1]
--1
-
-[0,-1,2,-3,null,null,4]
--1
-
-[0,-1,2,-3,null,null,4]
--4
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    bool find(TreeNode* root, int k, unordered_set<int>&s){
+15        if(root == nullptr)return false;
+16        if(s.count(k-root->val))return true;
+17        s.insert(root->val);
+18        return find(root->left, k, s)||find(root->right, k, s);
+19    }
+20    bool findTarget(TreeNode* root, int k) {
+21        unordered_set<int>s;
+22        return find(root, k, s);
+23    }
+24};
