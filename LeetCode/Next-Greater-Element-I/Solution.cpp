836956b8f1b2cbac6044c471f7e0a7de2f49dc62
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2026-03-14 15:55:31 +0530

    Add solution for Next Greater Element I

--- LeetCode/Next-Greater-Element-I/Solution.cpp
+++ LeetCode/Next-Greater-Element-I/Solution.cpp
@@ -2,8 +2,8 @@
 2public:
 3    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
 4        stack<int> st;
-5        unordered_map<int, int> mpp;
-6        for( int num : nums2){
+5        unordered_map<int,int> mpp;
+6        for(int num : nums2){
 7            while(!st.empty() && num>st.top()){
 8                mpp[st.top()] = num;
 9                st.pop();
@@ -14,7 +14,7 @@
 14            mpp[st.top()] = -1;
 15            st.pop();
 16        }
-17        vector<int> result;
+17        vector<int>result;
 18        for(int num : nums1){
 19            result.push_back(mpp[num]);
 20        }
