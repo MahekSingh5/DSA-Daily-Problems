df662230393415d6ab188f2152adba41518db7b9
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2026-03-18 15:23:31 +0530

    Add solution for Next Greater Element II

--- LeetCode/Next-Greater-Element-II/Solution.cpp
+++ LeetCode/Next-Greater-Element-II/Solution.cpp
@@ -3,9 +3,9 @@
 3    vector<int> nextGreaterElements(vector<int>& nums) {
 4        int n = nums.size();
 5        stack<int>st;
-6        vector<int> result(n,-1);
+6        vector<int>result(n, -1);
 7        for(int i=0; i<2*n-1; i++){
-8            while(!st.empty() && nums[i%n] > nums[st.top()]){
+8            while(!st.empty() && nums[i%n]>nums[st.top()]){
 9                result[st.top()] = nums[i%n];
 10                st.pop();
 11            }
