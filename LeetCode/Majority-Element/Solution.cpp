7fa6f1de22fd9017279f5e8d8b66aedaa94a7323
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2026-06-19 21:30:08 +0530

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.cpp
+++ LeetCode/Majority-Element/Solution.cpp
@@ -2,10 +2,10 @@
 2public:
 3    int majorityElement(vector<int>& nums) {
 4        int n = nums.size();
-5        int freq = 0;
+5        int freq = 1;
 6        int majEle = nums[0];
-7        for(int i=0; i<n; i++){
-8            if(nums[i] == majEle) freq++;
+7        for(int i=1; i<n; i++){
+8            if(nums[i] == majEle)freq++;
 9            else if(nums[i] != majEle){
 10                freq--;
 11                if(freq<0){
@@ -13,7 +13,7 @@
 13                    majEle = nums[i];
 14                }
 15            }
-16        }
+16        } 
 17        return majEle;
 18    }
 19};
