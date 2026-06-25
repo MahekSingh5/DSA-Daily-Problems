a44fc4d8135b10d88c47d60c1989e724b03aef73
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2026-06-25 10:34:52 +0530

    Add solution for Single Number

--- LeetCode/Single-Number/Solution.java
+++ LeetCode/Single-Number/Solution.java
@@ -3,15 +3,8 @@
 3    int singleNumber(vector<int>& nums) {
 4        int ans = 0;
 5        for(int val : nums){
-6            ans ^= val; 
+6            ans ^= val;
 7        }
 8        return ans;
 9    }
 10};
-11/* Example:
-12Input: nums = [4,1,2,1,2]
-13Output: 4
-14
-15ans = 0^4^1^2^1^2 = 4
-16*/
-17
