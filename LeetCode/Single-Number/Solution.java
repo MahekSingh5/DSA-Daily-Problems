6327eb318ce1ea75f1864226bb43e937a1706884
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2026-06-25 10:43:48 +0530

    Add solution for Single Number

--- LeetCode/Single-Number/Solution.java
+++ LeetCode/Single-Number/Solution.java
@@ -1,10 +1,16 @@
 1class Solution {
-2public:
-3    int singleNumber(vector<int>& nums) {
-4        int ans = 0;
-5        for(int val : nums){
-6            ans ^= val;
-7        }
-8        return ans;
-9    }
-10};
+2    public int singleNumber(int[] nums) {
+3        int n = nums.length;
+4        for(int i=0; i<n; i++){
+5            int num = nums[i];
+6            int count = 0;
+7            for(int j=0; j<n; j++){
+8                if(nums[j] == num){
+9                    count++;
+10                }
+11            }
+12            if(count == 1)return num;
+13        }
+14        return -1;
+15    }
+16}
