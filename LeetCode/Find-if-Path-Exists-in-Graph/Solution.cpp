ec147ff67272f52690e7d7c889815b8a8111a874
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2026-07-15 01:50:55 +0530

    Add solution for Find if Path Exists in Graph

---
+++ LeetCode/Find-if-Path-Exists-in-Graph/Solution.cpp
@@ -0,0 +1,24 @@
+1class Solution {
+2public:
+3    bool dfs(int source, int destination, vector<vector<int>>&adj, vector<bool>&visited){
+4        if(source == destination)return true;
+5        visited[source] = true;
+6        for(int neigh: adj[source]){
+7            if(!visited[neigh]){
+8                if(dfs(neigh, destination, adj, visited)){
+9                    return true;
+10                }
+11            }
+12        }
+13        return false;
+14    }
+15    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+16        vector<vector<int>>adj(n);
+17        for(auto &e : edges){
+18            adj[e[0]].push_back(e[1]);
+19            adj[e[1]].push_back(e[0]);
+20        }
+21        vector<bool>visited(n, false);
+22        return dfs(source, destination, adj, visited );
+23    }
+24};
