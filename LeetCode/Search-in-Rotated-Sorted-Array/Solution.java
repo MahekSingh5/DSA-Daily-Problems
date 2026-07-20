478fa04ea5063ace426da82399b4f69cd1e7f46c
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2026-07-20 14:44:13 +0530

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
@@ -1,26 +1,23 @@
 1class Solution {
-2public:
-3    int search(vector<int>& nums, int target) {
-4        int st=0, end = nums.size()-1;
-5        while(st<=end){
-6            int mid = st+(end - st)/2;
-7            if(nums[mid] == target){
-8                return mid;
-9            }
-10            if(nums[st]<=nums[mid]){ //left sorted
-11                if(nums[st]<=target && target<nums[mid]){
-12                    end = mid-1;
-13                }else{
-14                    st = mid+1;
-15                }
-16            }else{//right sorted
-17                if(nums[mid]<=target && target<=nums[end]){
-18                    st = mid+1;
-19                }else{
-20                    end = mid-1;
-21                }
-22            }
-23        }
-24        return -1;
-25    }
-26};
+2    public int search(int[] nums, int target) {
+3        int st = 0, end = nums.length-1;
+4        while(st<=end){
+5            int mid = st + (end-st)/2;
+6            if(nums[mid] == target)return mid;
+7            if(nums[st] <= nums[mid]){
+8                if((target <= nums[mid] && target >= nums[st])){
+9                    end =  mid - 1;
+10                }else{
+11                    st = mid + 1;
+12                }
+13            }else{
+14                if(target >= nums[mid] && target <= nums[end]){
+15                    st = mid + 1;
+16                }else{
+17                    end = mid -1 ;
+18                }
+19            }
+20        }
+21        return -1;
+22    }
+23}
