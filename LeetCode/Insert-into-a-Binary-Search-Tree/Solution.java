f563512b4edb53cf738fd95e26d887d4ce883b2e
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2026-07-31 15:56:29 +0530

    Add solution for Insert into a Binary Search Tree

--- LeetCode/Insert-into-a-Binary-Search-Tree/Solution.java
+++ LeetCode/Insert-into-a-Binary-Search-Tree/Solution.java
@@ -1,23 +1,36 @@
 1/**
 2 * Definition for a binary tree node.
-3 * public class TreeNode {
+3 * struct TreeNode {
 4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public TreeNode insertIntoBST(TreeNode root, int val) {
-18        if(root == null)return new TreeNode(val);
-19        if(root.val<val)root.right = insertIntoBST(root.right,val);
-20        else root.left = insertIntoBST(root.left, val);
-21        return root;
-22    }
-23}
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    TreeNode* insertIntoBST(TreeNode* root, int val) {
+15        if(!root)return new TreeNode(val);
+16        TreeNode * curr = root;
+17        while(true){
+18            if(val < curr->val){
+19            if(curr->left){
+20                curr = curr->left;
+21            }else{
+22                curr->left = new TreeNode(val);
+23                break;
+24            }
+25        }else{
+26            if(curr->right){
+27                curr = curr->right;
+28            }else{
+29                curr->right = new TreeNode(val);
+30                break;
+31            }
+32        }
+33        }
+34        return root;
+35    }
+36};
