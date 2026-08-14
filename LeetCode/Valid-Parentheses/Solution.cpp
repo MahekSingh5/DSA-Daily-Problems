842ee5ab7e508fa35633c87ad5ae75e119dae174
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2026-08-14 22:37:30 +0530

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.cpp
+++ LeetCode/Valid-Parentheses/Solution.cpp
@@ -2,18 +2,22 @@
 2public:
 3    bool isValid(string s) {
 4        stack<char> st;
-5        for(char c : s){
-6            if(c == '(' || c == '[' || c == '{')st.push(c);
-7            else{
-8                if(st.empty())return false;
-9                char top = st.top();
-10                st.pop();
-11                if((c == ')' && top != '(') ||
-12                    (c == ']' && top != '[') ||
-13                    (c == '}' && top != '{')
-14                )return false;
-15            }
-16        }
-17        return (st.empty());
-18    }
-19};
+5        for(char ch : s){
+6            if(ch == '(' || ch == '{' || ch == '['){
+7                st.push(ch);
+8            }else{
+9                if(st.empty())return false;
+10                if((ch == ')' && st.top() != '(') || 
+11                (ch == '}' && st.top() != '{')||
+12                (ch == ']' && st.top() != '[')){
+13                    return false;
+14                }else{
+15                    st.pop();
+16                }
+17            }
+18        }
+19    return st.empty();
+20    }
+21};
+22
+23
