12310a115de43cc77f889fa584930d88edd82e5e
MahekSingh5 <152600399+MahekSingh5@users.noreply.github.com>
2026-08-14 22:52:43 +0530

    Add solution for Maximum Subarray

--- LeetCode/Maximum-Subarray/Solution.cpp
+++ LeetCode/Maximum-Subarray/Solution.cpp
@@ -1,14 +1,13 @@
 1class Solution {
 2public:
 3    int maxSubArray(vector<int>& nums) {
-4        int currentSum = 0; int maxsum = INT_MIN;
-5        for(int val : nums){
-6             currentSum += val;
-7             maxsum = max(currentSum, maxsum);
-8             if(currentSum < 0){
-9                currentSum = 0;
-10             }
-11        }
-12        return maxsum;
-13    }
-14};
+4        int maxSum = INT_MIN;
+5        int currSum = 0;
+6        for(int i = 0; i<nums.size(); i++){
+7            currSum += nums[i];
+8            maxSum = max(maxSum, currSum);
+9            if(currSum < 0)currSum = 0;
+10        }
+11        return maxSum;
+12    }
+13};
